488c1ffd257fdc19167def7449d09172816a83cc
Shakiba Alam <shakibaalam092@gmail.com>
2022-09-03 20:46:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//dependencies
+const express = require("express");
+const cors = require("cors");
+const { errorHandler } = require("./middlewares/errorHandler");
+const usersRoutes = require("./routes/usersRoutes/users.routes");
+
+//app config
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//handle application errors
+app.use(errorHandler)
+
+// dynamic api routes
+app.use("/api/v1", usersRoutes );
+
+//create server
+app.listen(port, () => console.log(`Listening on Port: ${port}`));
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log({
+    name: err.name,
+    message: err.message,
+    stack: err.stack,
+  });
+  // close server & exit process
+  app.close(() => process.exit(1));
+});
